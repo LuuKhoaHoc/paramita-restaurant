d06bfb4ba3d5b30c8986cb4a156d189f8b87f071
LuuKhoaHoc_PC <lkhoc200443@gmail.com>
2024-01-21 23:48:23 +0700

    Refactor Contact component to be responsive

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -22,8 +22,10 @@ import {
   InstagramLogo,
   PaperPlaneTilt
 } from '@phosphor-icons/react'
+import { useResponsive } from '~/utils/responsive'
 
 const Contact = () => {
+  const { isMobile, isTablet, isLaptop } = useResponsive()
   const textColor = useThemeModeValue('#371b04', '#d1e9d5')
   const [scrollEvent, setScrollEvent] = useState(false)
   useEffect(() => {
@@ -44,49 +46,52 @@ const Contact = () => {
       />
       <Box w={'100%'} h={'100%'} pos={'relative'}>
         <Grid templateColumns={12}>
-          <Grid.Item columnStart={3} columnEnd={11}>
-            <Flex pos={'relative'} gap={fr(8)} direction='column' my={fr(5)}>
-              <Flex direction='row' justify='around' gap={fr(8)}>
-                <Box w={fr(100)}>
-                  <Text as={'h1'} mb={fr(14)}>
+          <Grid.Item
+            columnStart={isTablet ? 2 : isMobile ? 1 : 3}
+            columnEnd={isTablet ? 12 : isMobile ? 13 : 11}
+          >
+            <Flex
+              pos={'relative'}
+              gap={isMobile ? fr(5) : fr(8)}
+              direction='row'
+              mx={isMobile ? fr(3) : 0}
+              my={fr(5)}
+            >
+              <Flex direction='column' gap={fr(8)}>
+                <Box>
+                  <Text
+                    as={'h1'}
+                    fs={isTablet ? 'xl' : isMobile ? 'lg' : 'inherit'}
+                    mb={fr(14)}
+                  >
                     Liên hệ
                   </Text>
-                  <Text as={'p'} fs={'xl'} tt={'capitalize'}>
+                  <Text
+                    as={'p'}
+                    fs={isTablet ? 'md' : isMobile ? 'base' : 'xl'}
+                    tt={'capitalize'}
+                  >
                     107 Nguyễn Thị Minh Khai, phường Bến Nghé, quận 1, TP.HCM
                   </Text>
-                  <Text as={'p'} fs={'xl'} tt={'capitalize'}>
+                  <Text
+                    as={'p'}
+                    fs={isTablet ? 'md' : isMobile ? 'base' : 'xl'}
+                    tt={'capitalize'}
+                  >
                     108 Lê Văn Sỹ, phường 13, quận 3, TP.HCM
                   </Text>
                 </Box>
-                <Box w={fr(100)}>
-                  <Text as={'h2'} mb={fr(8)}>
-                    Số điện thoại:{' '}
-                    <Link
-                      href='tel:0123456789'
-                      cl={[textColor, { hover: 'primary' }]}
-                    >
-                      +84 (0)123-456-789
-                    </Link>
-                  </Text>
-                  <Text as={'h2'} mb={fr(8)}>
-                    Email:{' '}
-                    <Link
-                      href='mailto:hi@paramita.com'
-                      cl={[textColor, { hover: 'primary' }]}
-                    >
-                      hi@paramita.com
-                    </Link>
-                  </Text>
-                </Box>
-              </Flex>
-              <Flex direction='row' justify='around' gap={fr(8)}>
-                <Box w={fr(100)}>
-                  <Text as={'h1'} mb={fr(8)}>
+                <Box>
+                  <Text
+                    as={'h1'}
+                    fs={isTablet ? 'xl' : isMobile ? 'lg' : 'inherit'}
+                    mb={fr(8)}
+                  >
                     Follow Us
                   </Text>
                   <Link
                     w={'fit-content'}
-                    fs={'xl'}
+                    fs={isTablet ? 'md' : isMobile ? 'base' : 'xl'}
                     href='https://www.facebook.com/'
                     cl={[textColor, { hover: 'diamond' }]}
                   >
@@ -97,7 +102,7 @@ const Contact = () => {
                   </Link>
                   <Link
                     w={'fit-content'}
-                    fs={'xl'}
+                    fs={isTablet ? 'md' : isMobile ? 'base' : 'xl'}
                     href='https://www.instagram.com'
                     cl={[textColor, { hover: 'ruby' }]}
                   >
@@ -107,23 +112,81 @@ const Contact = () => {
                     Instagram
                   </Link>
                 </Box>
-                <Box w={fr(100)}>
-                  <Text as={'h1'} mb={fr(8)}>
+              </Flex>
+              <Flex direction='column' gap={fr(8)}>
+                <Box>
+                  <Text
+                    as={'h2'}
+                    fs={isTablet ? 'md' : isMobile ? 'base' : 'xl'}
+                    mb={fr(8)}
+                  >
+                    Số điện thoại:{' '}
+                    <Link
+                      href='tel:0123456789'
+                      cl={[textColor, { hover: 'primary' }]}
+                    >
+                      +84 (0)123-456-789
+                    </Link>
+                  </Text>
+                  <Text
+                    as={'h2'}
+                    fs={isTablet ? 'md' : isMobile ? 'base' : 'xl'}
+                    mb={fr(8)}
+                  >
+                    Email:{' '}
+                    <Link
+                      href='mailto:hi@paramita.com'
+                      cl={[textColor, { hover: 'primary' }]}
+                    >
+                      hi@paramita.com
+                    </Link>
+                  </Text>
+                </Box>
+
+                <Box>
+                  <Text
+                    as={'h1'}
+                    fs={isTablet ? 'xl' : isMobile ? 'lg' : 'inherit'}
+                    mb={fr(8)}
+                  >
                     Giờ mở cửa
                   </Text>
-                  <Text as={'p'} fs={'xl'} tt={'capitalize'}>
+                  <Text
+                    as={'p'}
+                    fs={isTablet ? 'md' : isMobile ? 'base' : 'xl'}
+                    tt={'capitalize'}
+                  >
                     Thứ 2 - Chủ nhật
                   </Text>
-                  <Text as={'p'} fs={'xl'} tt={'capitalize'}>
+                  <Text
+                    as={'p'}
+                    fs={isTablet ? 'md' : isMobile ? 'base' : 'xl'}
+                    tt={'capitalize'}
+                  >
                     9:00 - 22:00
                   </Text>
                 </Box>
               </Flex>
             </Flex>
-            <Flex pos={'relative'} gap={fr(8)} direction='row' my={fr(5)}>
+            <Flex
+              pos={'relative'}
+              gap={isMobile ? fr(5) : fr(8)}
+              direction='row'
+              mx={isMobile ? fr(3) : 0}
+              my={fr(5)}
+            >
               <Box w={'30%'}>
-                <Text as={'h1'}>LIÊN HỆ VỚI CHÚNG TÔI</Text>
-                <Text as={'p'} fs={'xl'} mt={fr(4)}>
+                <Text
+                  as={'h1'}
+                  fs={isTablet ? 'xl' : isMobile ? 'lg' : 'inherit'}
+                >
+                  LIÊN HỆ VỚI CHÚNG TÔI
+                </Text>
+                <Text
+                  as={'p'}
+                  fs={isTablet ? 'md' : isMobile ? 'base' : 'xl'}
+                  mt={fr(4)}
+                >
                   Paramita luôn sẵn sàng hỗ trợ mọi thắc mắc cũng như góp ý của
                   quý khách hàng. Có bất kỳ câu hỏi hoặc yêu cầu nào, vui lòng
                   điền các thông tin vào form bên cạnh. Chúng tôi sẽ phản hồi
@@ -137,37 +200,34 @@ const Contact = () => {
                 sx={{
                   '.PrismaneTextField-label, .PrismaneTextarea-label': {
                     fontSize: '20px'
+                  },
+                  '@media (max-width: 768px)': {
+                    '.PrismaneTextField-label, .PrismaneTextarea-label': {
+                      fontSize: '16px'
+                    }
                   }
                 }}
               >
-                <Form onSubmit={(e) => e.preventDefault()}>
+                <Form w={'100%'} onSubmit={(e) => e.preventDefault()}>
                   <TextField
                     label='Họ và tên'
                     variant='outlined'
-                    size='md'
                     placeholder='Nguyễn Văn A'
                   />
                   <TextField
                     label='Email'
                     variant='outlined'
-                    size='md'
                     placeholder='hi@paramita.com'
                   />
                   <TextField
                     label='Số điện thoại'
                     variant='outlined'
-                    size='md'
                     placeholder='0123456789'
                   />
-                  <TextareaField
-                    label='Thông tin'
-                    variant='outlined'
-                    size='md'
-                  />
+                  <TextareaField label='Thông tin' variant='outlined' />
                   <Button
                     className='GeomanistMedium-font'
                     type='submit'
-                    size='md'
                     iconPosition='right'
                     icon={<PaperPlaneTilt />}
                   >
