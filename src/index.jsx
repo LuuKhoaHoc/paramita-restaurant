644a3dbad8d5279a5f2a7412eafd0823dbad5a8d
luukhoahoc <lkhoc200443@gmail.com>
2024-02-27 00:27:24 +0700

    Add Apollo Client and GraphQL dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { PrismaneProvider } from '@prismane/core'
 import { BrowserRouter } from 'react-router-dom'
 import theme from './theme'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <>
     <BrowserRouter>
       <PrismaneProvider theme={theme}>
+        <ApolloProvider client={client}>
         <App />
+        </ApolloProvider>
       </PrismaneProvider>
     </BrowserRouter>
   </>
